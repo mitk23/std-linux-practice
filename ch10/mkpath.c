ef36eb27e6f5ff998a614536b1f320e5e6909a3b
Itsuki Matsunaga <mitk232@gmail.com>
2022-04-28 19:25:06 +0900

    completed chap10 exercises

--- ch10/mkpath.c
+++ ch10/mkpath.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <string.h>
+#include <unistd.h>
 #include <errno.h>
 
 static void make_path(const char *path);
@@ -13,59 +13,56 @@ int main(int argc, char *argv[]) {
 	int i;
 
 	if (argc < 2) {
-		fprintf(stderr, "Usage: %s <dir>", argv[0]);
+		fprintf(stderr, "%s: file name not given\n", argv[0]);
 		exit(1);
 	}
 
-	for (i = 1; i < argc; ++i) make_path(argv[i]);
-
+	for (i = 1; i < argc; ++i) {
+		make_path(argv[i]);
+	}
 	exit(0);
 }
 
+
 static void make_path(const char *path) {
 	if (mkdir(path, 0777) == 0) return;
-
 	if (errno == EEXIST) {
 		struct stat st;
-		if (stat(path, &st) < 0) die("stat");
+		if (stat(path, &st) < 0) die("stat(2)");
 		if (!S_ISDIR(st.st_mode)) {
 			fprintf(stderr, "file exists but is not a directory: %s\n", path);
 			exit(1);
 		}
 		return;
 	} else if (errno == ENOENT) {
-		char *parent = strdup(path);
-		if (!parent) die("strdup");
+		char *parent_path = strdup(path);
+		if (!parent_path) die("strdup(3)");
 
-		char *last = parent + strlen(parent) - 1;
-		while (*last == '/' && last != parent) {
-			*last = '\0';
-			--last;
+		char *last = parent_path + strlen(parent_path) - 1;
+		while (*last == '/' && last != parent_path) {
+			*last-- = '\0';
 		}
 
-		if (strcmp(parent, "/") == 0) {
+		if (strcmp(parent_path, "/") == 0) {
 			fprintf(stderr, "error: root directory is not a directory???\n");
 			exit(1);
 		}
 
-		char *sep = strrchr(parent, '/');
+		char *sep = strrchr(parent_path, '/');
 		if (!sep) {
-			fprintf(stderr, "error: current directory is not a directory???");
-			exit(1);
-		} else if (sep == parent) {
-			fprintf(stderr, "error: root directory is not a directory???");
+			fprintf(stderr, "error: current directory is not a directory???\n");
 			exit(1);
 		}
-
 		*sep = '\0';
 
-		make_path(parent);
+		make_path(parent_path);
 		if (mkdir(path, 0777) < 0) die(path);
 		return;
 	} else {
 		die(path);
-	}	
+	}
 }
+
 
 static void die(const char *s) {
 	perror(s);
--- ch10/traverse.c
+++ ch10/traverse.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
 #include <errno.h>
 
 struct strbuf {
@@ -17,112 +17,114 @@ static struct strbuf *strbuf_new(void);
 static void strbuf_realloc(struct strbuf *buf, size_t size);
 static void print_error(char *s);
 
-static char *prog_name;
 
 int main(int argc, char *argv[]) {
 	struct strbuf *pathbuf;
 
 	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <dir>\n", argv[0]);
+		fprintf(stderr, "Usage: %s <div>\n", argv[0]);
 		exit(1);
 	}
-	prog_name = argv[0];
 	pathbuf = strbuf_new();
 	strbuf_realloc(pathbuf, strlen(argv[1]));
 	strcpy(pathbuf->ptr, argv[1]);
-	
 	traverse(pathbuf);
-
 	exit(0);
 }
 
-static void traverse(struct strbuf *buf) {
+
+static void traverse(struct strbuf *pathbuf) {
 	DIR *d;
 	struct dirent *ent;
 	struct stat st;
 
-	d = opendir(buf->ptr);
+	d = opendir(pathbuf->ptr);
 	if (!d) {
 		switch (errno) {
-		case ENOTDIR:
-			return;
-		case ENOENT:
-			return;
-		case EACCES:
-			puts(buf->ptr);
-			print_error(buf->ptr);
-			return;
-		default:
-			print_error(buf->ptr);
-			exit(1);
+			case ENOTDIR:
+				return;
+			case ENOENT:
+				return;
+			case EACCES:
+				puts(pathbuf->ptr);
+				print_error(pathbuf->ptr);
+				return;
+			default:
+				print_error(pathbuf->ptr);
+				exit(1);
 		}
 	}
 
-	puts(buf->ptr);
-	while ((ent = readdir(d))) {
+	puts(pathbuf->ptr);
+	while (ent = readdir(d)) {
 		if (strcmp(ent->d_name, ".") == 0) continue;
 		if (strcmp(ent->d_name, "..") == 0) continue;
-		strbuf_realloc(buf, buf->len + 1 + strlen(ent->d_name) + 1);
 
-		// special handling for the root
-		if (strcmp(ent->d_name, "/") != 0) {
-			strcat(buf->ptr, "/");
+		strbuf_realloc(pathbuf, pathbuf->len + 1 + strlen(ent->d_name) + 1);
+		if (strcmp(pathbuf->ptr, "/") != 0) {
+			strcat(pathbuf->ptr, "/");
 		}
-		strcat(buf->ptr, ent->d_name);
+		strcat(pathbuf->ptr, ent->d_name);
 
-		if (lstat(buf->ptr, &st) < 0) {
+		if (lstat(pathbuf->ptr, &st) < 0) {
 			switch (errno) {
-			case ENOENT:
-				break;
-			case EACCES:
-				print_error(buf->ptr);
-				exit(1);
+				case ENOENT:
+					break;
+				case EACCES:
+					print_error(pathbuf->ptr);
+					break;
+				default:
+					print_error(pathbuf->ptr);
+					exit(1);
 			}
 		} else {
 			if (S_ISDIR(st.st_mode) && !S_ISLNK(st.st_mode)) {
-				traverse(buf);
+				traverse(pathbuf);
 			} else {
-				puts(buf->ptr);
+				puts(pathbuf->ptr);
 			}
 		}
-		*strrchr(buf->ptr, '/') = '\0';
+		*strrchr(pathbuf->ptr, '/') = '\0';
 	}
-	closedir(d);
-}
+	
+	if (closedir(d) < 0) {
+		perror(pathbuf->ptr);
+		exit(1);
+	}
+}	
 
 #define INIT_LEN 1024
 
-static struct strbuf* strbuf_new(void) {
-	struct strbuf *buf;
+static struct strbuf *strbuf_new(void) {
+       struct strbuf *buf;
 
-	buf = (struct strbuf*)malloc(sizeof(struct strbuf));
-	if (!buf) {
-		print_error("malloc(3)");
-		exit(1);
-	}
-	buf->ptr = (char*)malloc(INIT_LEN);
-	if (!buf->ptr) {
-		print_error("malloc(3)");
-		exit(1);
-	}
-	buf->len = INIT_LEN;
-	return buf;
-}
+       buf = (struct strbuf*)malloc(sizeof(struct strbuf));
+       if (!buf) {
+	       print_error("malloc(3)");
+	       exit(1);
+       }
+       buf->ptr = malloc(INIT_LEN);
+       if (!buf->ptr) {
+	       print_error("malloc(3)");
+	       exit(1);
+       }
+       buf->len = INIT_LEN;
+       return buf;
+}       
 
-static void strbuf_realloc(struct strbuf *buf, size_t size) {
+static void strbuf_realloc(struct strbuf *buf, size_t len) {
 	char *tmp;
-	
-	if (buf->len > size) return;
 
-	tmp = realloc(buf->ptr, size); 
+	if (buf->len > len) return;
+	tmp = realloc(buf->ptr, len);
 	if (!tmp) {
-		print_error("malloc(3)");
+		print_error("realloc(3)");
 		exit(1);
 	}
 	buf->ptr = tmp;
-	buf->len = size;
+	buf->len = len;
 }
 
 static void print_error(char *s) {
-	fprintf(stderr, "%s: %s: %s\n", prog_name, s, strerror(errno));
+	fprintf(stderr, "%s: %s\n", s, strerror(errno));
 }
